998f11e8c2b8491d48e87ec5764df98dc55aeceb
Jonathan <jonathanhutagalung02@gmail.com>
2026-04-22 13:33:25 +0700

    Add transaction.js

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,8 +1,36 @@
 const express = require('express');
 
+const router = express.Router();
+const Transaction = require('../models/transaction');
 const books = require('./components/books/books-route');
 const users = require('./components/users/users-route');
 const accountsRoutes = require('./components/accounts/accounts-route');
+
+router.post('/transaction', async (req, res) => {
+  const data = await Transaction.create(req.body);
+  res.json(data);
+});
+
+router.get('/transaction/:id/status', async (req, res) => {
+  try {
+    const trx = await Transaction.findById(req.params.id);
+
+    if (!trx) {
+      return res.status(404).json({
+        message: 'Transaksi tidak ditemukan',
+      });
+    }
+
+    return res.json({
+      id: trx.id,
+      status: trx.status,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 module.exports = () => {
   const app = express.Router();
@@ -10,7 +38,7 @@ module.exports = () => {
   books(app);
   users(app);
 
-  app.use(accountsRoutes); // ← TAMBAH DI SINI
+  app.use(accountsRoutes);
 
   return app;
 };
