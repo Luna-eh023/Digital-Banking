9637e34c2e25183cca2c1d9f2d2ba70a292f6d7c
Laudy Sildy <laudysildy86@gmail.com>
2026-04-24 15:06:50 +0700

    //fix transaction api and probably package aswell

--- src/api/routes.js
+++ src/api/routes.js
@@ -5,21 +5,23 @@ const users = require('./components/users/users-route');
 const otp = require('./components/OTP/OTP-Routes');
 const card = require('./components/card/card-routes');
 const sms = require('./components/SMS/SMS-Route');
+
 const accountsRoutes = require('./components/accounts/accounts-route');
 const transactionsRoutes = require('./components/transactions/transaction-route');
 
 module.exports = () => {
   const app = express.Router();
 
+  // ROUTES STYLE FUNCTION
   books(app);
   users(app);
   otp(app);
   card(app);
-  transactionsRoutes(app);
   sms(app);
 
+  // ROUTES STYLE ROUTER
   app.use(accountsRoutes);
-  app.use(transactionRoutes);
+  app.use(transactionsRoutes); // ✅ FIX DI SINI
 
   return app;
 };
